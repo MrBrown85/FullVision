81c893b1049bc449e828538b044bb8b600890d5c
Your Name <you@example.com>
2026-04-18 17:42:25 -0700

    Bump sw.js cache to v35 for canonical reads deploy

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // IMPORTANT: Increment the version number when deploying new app code
 // This ensures users get the latest files
-const CACHE_NAME = 'fullvision-v34';
+const CACHE_NAME = 'fullvision-v35';
 
 // All app files to pre-cache on install
 const PRECACHE_URLS = [
